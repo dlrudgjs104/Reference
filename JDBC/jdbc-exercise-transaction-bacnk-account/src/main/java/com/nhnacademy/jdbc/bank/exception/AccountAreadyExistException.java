313e85eb5869fb9235d2ab3641112a672eeee0f5
dlrudgjs104 <dlrudgjs104@naver.com>
2024-05-02 17:07:23 +0900

    bank account 연습 삭제

--- JDBC/jdbc-exercise-transaction-bacnk-account/src/main/java/com/nhnacademy/jdbc/bank/exception/AccountAreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.jdbc.bank.exception;
-
-public class AccountAreadyExistException extends RuntimeException {
-    public AccountAreadyExistException(long accountNumber) {
-        super("account aready exist accountNumber : "+ accountNumber );
-    }
-}
--- JDBC/jdbc-exercise-transaction-bacnk-account/src/main/java/com/nhnacademy/jdbc/bank/exception/AccountNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.jdbc.bank.exception;
-
-public class AccountNotFoundException extends RuntimeException {
-    public AccountNotFoundException(long accountNumber) {
-        super("Account Not Found accountNumber : " + accountNumber );
-    }
-}
